fe1ac29d8dccb6319c8c89a1646568cd3d1bc1c2
星梦StarsDream <starsdream00@icloud.com>
2023-01-09 00:51:46 +0800

    初始化

---
+++ src/Main.cs
@@ -0,0 +1,26 @@
+﻿using LiteLoader.Hook;
+using LiteLoader.NET;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Ptrarc;
+[PluginMain("RandomSeed")]
+public class RandomSeed : IPluginInitializer
+{
+    public string Introduction => "随机种子";
+    public Dictionary<string, string> MetaData => new();
+    public Version Version => new(1, 0, 0);
+    public void OnInitialize() => Thook.RegisterHook<RandomSeedHook, RandomSeedHookDelegate>();
+}
+
+internal delegate void RandomSeedHookDelegate(nint a1, nint a2);
+[HookSymbol("?write@StartGamePacket@@UEBAXAEAVBinaryStream@@@Z")]
+internal class RandomSeedHook : THookBase<RandomSeedHookDelegate>
+{
+    public override RandomSeedHookDelegate Hook => (a1, a2) =>
+    {
+        Marshal.WriteInt64(a1, 48, Random.Shared.NextInt64());
+        Original(a1, a2);
+    };
+}
