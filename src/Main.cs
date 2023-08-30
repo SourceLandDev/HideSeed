8ef51883f54c3298cc612e4a88a228958afbc8b3
星梦 <starsdream00@icloud.com>
2023-08-30 11:54:21 +0800

    修复

--- src/Main.cs
+++ src/Main.cs
@@ -1,15 +1,24 @@
+using LiteLoader.Event;
 using LiteLoader.Hook;
 using LiteLoader.NET;
 using System.Runtime.InteropServices;
 
-namespace Ptrarc;
+namespace RandomSeed;
 [PluginMain("RandomSeed")]
-public class RandomSeed : IPluginInitializer
+public sealed class Main : IPluginInitializer
 {
     public string Introduction => "随机种子";
     public Dictionary<string, string> MetaData => new();
-    public Version Version => new();
-    public void OnInitialize() => Thook.RegisterHook<RandomSeedHook, RandomSeedHookDelegate>();
+    internal static long SeedCache { get; private set; }
+    public void OnInitialize()
+    {
+        PlayerPreJoinEvent.Event += e =>
+        {
+            SeedCache = new Random(e.Player.ClientId.GetHashCode()).NextInt64();
+            return true;
+        };
+        Thook.RegisterHook<RandomSeedHook, RandomSeedHookDelegate>();
+    }
 }
 
 internal delegate void RandomSeedHookDelegate(nint a1, nint a2);
@@ -18,7 +27,7 @@ internal class RandomSeedHook : THookBase<RandomSeedHookDelegate>
 {
     public override RandomSeedHookDelegate Hook => (a1, a2) =>
     {
-        Marshal.WriteInt64(a1, 48, Random.Shared.NextInt64());
+        Marshal.WriteInt64(a1, 48, Main.SeedCache);
         Original(a1, a2);
     };
 }
