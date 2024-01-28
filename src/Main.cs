bcb96be080ca399223f326c451fdd165beb64339
予纾 <starsdream00@icloud.com>
2024-01-28 13:08:26 +0800

    修复

--- src/Main.cs
+++ src/Main.cs
@@ -3,31 +3,25 @@ using LiteLoader.Hook;
 using LiteLoader.NET;
 using System.Runtime.InteropServices;
 
-namespace RandomSeed;
-[PluginMain("RandomSeed")]
+namespace HiddenSeed;
+[PluginMain("HiddenSeed")]
 public sealed class Main : IPluginInitializer
 {
-    public string Introduction => "随机种子";
+    public string Introduction => "隐藏种子";
     public Dictionary<string, string> MetaData => new();
-    internal static long SeedCache { get; private set; }
     public void OnInitialize()
     {
-        PlayerPreJoinEvent.Event += e =>
-        {
-            SeedCache = new Random(e.Player.ClientId.GetHashCode()).NextInt64();
-            return true;
-        };
-        Thook.RegisterHook<RandomSeedHook, RandomSeedHookDelegate>();
+        Thook.RegisterHook<WriteStartGamePacketHook, WriteStartGamePacketHookDelegate>();
     }
 }
 
-internal delegate void RandomSeedHookDelegate(nint a1, nint a2);
+internal delegate void WriteStartGamePacketHookDelegate(nint @this, nint a2);
 [HookSymbol("?write@StartGamePacket@@UEBAXAEAVBinaryStream@@@Z")]
-internal class RandomSeedHook : THookBase<RandomSeedHookDelegate>
+internal class WriteStartGamePacketHook : THookBase<WriteStartGamePacketHookDelegate>
 {
-    public override RandomSeedHookDelegate Hook => (a1, a2) =>
+    public override WriteStartGamePacketHookDelegate Hook => (@this, a2) =>
     {
-        Marshal.WriteInt64(a1, 48, Main.SeedCache);
-        Original(a1, a2);
+        Marshal.WriteInt64(@this, 48, default);
+        Original(@this, a2);
     };
 }
